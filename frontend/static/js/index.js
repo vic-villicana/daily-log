69e9ffe3552ca693534f0a8d74d9020ed7e4b15a
vic-villicana <victor_u9@msn.com>
2021-06-09 19:38:49 -0700

    create router function

---
+++ frontend/static/js/index.js
@@ -0,0 +1,46 @@
+
+
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router()
+}
+
+const router = async () => {
+    const routes = [
+        {path:"/", view: () => console.log('viewing dashboard')},
+        {path:"/weeks", view: () => console.log('weeks')},
+        {path:"/months", view: () => console.log('months')}
+    ]
+
+
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path 
+        }
+    })
+
+    let match = potentialMatches.find(potential => potential.isMatch)
+
+
+    if(!match){
+        match = {
+            route: routes[0],
+            isMatch: true
+        }
+    }
+    console.log(match)
+}
+
+
+document.body.addEventListener("click", e => {
+    if(e.target.matches('[data-link]')){
+        e.preventDefault()
+        
+        navigateTo(e.target.href)
+    }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    router()
+})
